7f774d76a29bb47b120237b68e644ad7ebdb5487
QuangVinh <vn449368@gamil.com>
2025-09-27 16:47:22 +0700

    hoanf thanhf chuwcs nawng ddawng nhaapj

--- app/src/main/java/com/example/selt_design/MainActivity.java
+++ app/src/main/java/com/example/selt_design/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.selt_design;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText etEmail, etPassword;
+    Button btnLogin, btnCreateAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnCreateAccount = findViewById(R.id.btnCreateAccount);
+
+        btnCreateAccount.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
+
+        btnLogin.setOnClickListener(v -> {
+            String email = etEmail.getText().toString();
+            String pass = etPassword.getText().toString();
+            // TODO: kiểm tra đăng nhập
+            Toast.makeText(this, "Email: " + email + "\nPassword: " + pass, Toast.LENGTH_SHORT).show();
         });
     }
 }
